ec4c7eb09b60f4851a6758ce2ad6cf9ffa9c3982
frugoz05 <adamdawidziuk@onet.pl>
2024-01-05 20:10:57 +0100

    Init

---
+++ app/src/main/java/com/example/foodder/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.foodder.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.foodder.presentation.main_screen.MainScreen
+import com.example.foodder.presentation.ui.theme.FoodderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FoodderTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foodder/presentation/main_screen/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.example.foodder.presentation.main_screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text(text = "Hello world")
+}
